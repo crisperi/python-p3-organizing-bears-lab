9067cdc8ec7e2ec2f70c46dffe977c292e04556a
root <root@DESKTOP-G9245EU.>
2024-12-15 19:37:57 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color,temperament,alive)
+VALUES 
+    ('Mr. Chocolate', 35, 'M', 'Brown', 'Sweet', 1),
+    ('Rowdy', 30, 'M', 'Black', 'Aggressive', 1),
+    ('Tabitha', 28, 'F', 'Blonde', 'Calm', 1),
+    ('Sergeant Brown', 40, 'M', 'Brown', 'Disciplined', 1),
+    ('Melissa', 25, 'F', 'Red', 'Friendly', 1),
+    ('Grinch', 45, 'M', 'Green', 'Grumpy', 1),
+    ('Wendy', 32, 'F', 'Black', 'Curious', 1),
+    (NULL, 22, 'M', 'Grey', 'Mysterious', 1);
